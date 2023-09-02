bdc6af87293d646de80cdf06682c1129d50a9732
kasimkid <107450115+kasimkid@users.noreply.github.com>
2023-09-02 17:12:15 +0000

    creating table

--- src/models.py
+++ src/models.py
@@ -7,23 +7,62 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+class Favorite (Base):
+    __tablename__ = 'favorite'
+    favoriteID = Column(Integer, primary_key=True)
+    characterID = Column(Integer,ForeignKey('character.characterID'))
+    nicknameID = Column(Integer, ForeignKey('register.registerID'))
+
+
+class Register (Base):
+    __tablename__ = 'register'
+    registerID = Column(Integer,primary_key=True) 
+    name = Column(String(50),nullable=False)
+    password = Column(String(12),nullable=False)
+    email = Column (String(50), nullable=True, unique=True)
+    register = relationship('Favorite')
+
+class Character(Base):
+    __tablename__ = 'character'
+    characterID = Column(Integer,primary_key=True)
+    name = Column(String(50),nullable=False)
+    status = Column(String(50),nullable=False)
+    episode = Column(String(50),nullable=False)
+    location = Column(String(50),nullable=False)
+    favoriteID = relationship('Favorite')
+
+class Episode(Base):
+    __tablename__ = 'episode'
+    episodeID = Column(Integer, primary_key=True)
+    name = Column(String(50),nullable=False)
+    episode = Column(String(700),nullable=False)
+    favoriteID = relationship('Favorite')
+
+class Dimension (Base):
+    __tablename__ = 'dimension'
+    dimensionID = Column(Integer, primary_key=True)
+    favoriteID = relationship('Favorite')
+
+    
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
     def to_dict(self):
         return {}
